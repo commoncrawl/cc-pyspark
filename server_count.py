329f220bb8cdad974b20015b76fdeb285fe62ba2
Sebastian Nagel <sebastian@commoncrawl.org>
2022-08-23 13:26:20 +0200

    Sample job to count server names - bug fix for WARC/WAT records other than response records:   do not count these records as "(no server in HTTP header)" - WARC response records: count all unique values of multiple given   "Server" HTTP headers

--- server_count.py
+++ server_count.py
@@ -11,7 +11,13 @@ class ServerCountJob(CCSparkJob):
     fallback_server_name = '(no server in HTTP header)'
 
     def process_record(self, record):
-        server_name = None
+        # Notes:
+        # - HTTP headers may include multiple "Server" headers, often indicating
+        #   a delivery cascade, e.g. nginx + idlb1 (load balancer).
+        #   We iterate over all "Server" headers in WARC records, in order to count
+        #   multiple (but unique) "Server" headers.
+        # - WAT records store HTTP headers as JSON objects not preserving multiple
+        #   headers, see https://github.com/commoncrawl/ia-web-commons/issues/18
 
         if self.is_wat_json_record(record):
             # WAT (response) record
@@ -23,22 +29,40 @@ class ServerCountJob(CCSparkJob):
                                          ['Headers'] \
                                          ['Server'] \
                                          .strip()
+                    if server_name and server_name != '':
+                        yield server_name, 1
+                    else:
+                        yield ServerCountJob.fallback_server_name, 1
                 else:
                     # WAT request or metadata records
                     return
             except KeyError:
                 pass
+
         elif record.rec_type == 'response':
             # WARC response record
-            server_name = record.http_headers.get_header('server', None)
+            server_names = set()
+            for (name, value) in record.http_headers.headers:
+                if name.lower() == 'server':
+                    if value == '':
+                        pass
+                    elif value in server_names:
+                        self.get_logger().debug(
+                            "Not counting duplicated 'Server' header value for %s: %s",
+                            record.rec_headers.get_header('WARC-Target-URI'), value)
+                    else:
+                        yield value, 1
+                        server_names.add(value)
+            if not server_names:
+                yield ServerCountJob.fallback_server_name, 1
+            elif len(server_names) > 1:
+                self.get_logger().info(
+                    "Multiple 'Server' header values for %s: %s",
+                    record.rec_headers.get_header('WARC-Target-URI'), server_names)
+
         else:
             # warcinfo, request, non-WAT metadata records
             return
-
-        if server_name and server_name != '':
-            yield server_name, 1
-        else:
-            yield ServerCountJob.fallback_server_name, 1
 
 
 if __name__ == "__main__":
